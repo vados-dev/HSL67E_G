d65311f9656a6c1d271fb8872882bb44ecd48f1a
vados-dev <vados@vados.ru>
2025-01-14 16:04:16 +0300

    Initial commit

---
+++ omni_HSL67E_G.mk
@@ -0,0 +1,32 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from D41 device
+$(call inherit-product, device/sprd/omni_HSL67E_G/device.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+PRODUCT_DEVICE := HSL67E_G
+PRODUCT_NAME := omni_HSL67E_G
+PRODUCT_BRAND := 凌度
+PRODUCT_MODEL := HSL67E-G
+PRODUCT_MANUFACTURER := sprd
+
+PRODUCT_GMS_CLIENTID_BASE := android-sprd
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.build.security_patch=2099-12-31
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="sl8541e_cus_gofu_osea-user 8.1.0 OPM2.171019.012 08515 release-keys"
+
+BUILD_FINGERPRINT := SPRD/sl8541e_cus_gofu_osea/sl8541e_cus_go:8.1.0/OPM2.171019.012/08515:user/release-keys
