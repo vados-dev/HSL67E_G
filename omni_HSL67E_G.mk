9bf9a2d54c2bd092db14071517430d37661344c0
vados-dev <vados@vados.ru>
2025-01-15 08:04:40 +0300

    Update omni_HSL67E_G.mk

--- omni_HSL67E_G.mk
+++ omni_HSL67E_G.mk
@@ -10,7 +10,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 #$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from D41 device
-$(call inherit-product, device/sprd/omni_HSL67E_G/device.mk)
+$(call inherit-product, device/sprd/HSL67E_G/device.mk)
 
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
